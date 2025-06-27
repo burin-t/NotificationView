89f1f577676548b48545bcb5bb3d2b64cc4f59ca
burin-t <100820279+burin-t@users.noreply.github.com>
2025-06-27 18:17:43 +0700

    Update NotificationBannerView.swift

--- NotificationView/Classes/NotificationBannerView.swift
+++ NotificationView/Classes/NotificationBannerView.swift
@@ -173,7 +173,7 @@ class NotificationBannerView: UIView {
         self.addConstraints([
             NSLayoutConstraint(item: self, attribute: .leading, relatedBy: .equal, toItem: self.containerView, attribute: .leading, multiplier: 1, constant: -2),
             NSLayoutConstraint(item: self, attribute: .trailing, relatedBy: .equal, toItem: self.containerView, attribute: .trailing, multiplier: 1, constant: 2),
-            NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: self.containerView, attribute: .top, multiplier: 1, constant: 6).priority(900),
+            NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: self.containerView, attribute: .top, multiplier: 1, constant: -2).priority(900),
             NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal, toItem: self.containerView, attribute: .bottom, multiplier: 1, constant: 2).priority(900)
         ])
         
@@ -182,7 +182,7 @@ class NotificationBannerView: UIView {
         self.containerView.addConstraints([
             NSLayoutConstraint(item: self.containerView, attribute: .leading, relatedBy: .equal, toItem: self.headerView, attribute: .leading, multiplier: 1, constant: 0),
             NSLayoutConstraint(item: self.containerView, attribute: .trailing, relatedBy: .equal, toItem: self.headerView, attribute: .trailing, multiplier: 1, constant: 0),
-            NSLayoutConstraint(item: self.containerView, attribute: .top, relatedBy: .equal, toItem: self.headerView, attribute: .top, multiplier: 1, constant: 0)
+            NSLayoutConstraint(item: self.containerView, attribute: .top, relatedBy: .equal, toItem: self.headerView, attribute: .top, multiplier: 1, constant: 8)
         ])
         
         self.headerView.addConstraints([
